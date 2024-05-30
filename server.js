2d17c5fe406b81c55e20ee32c0c7739991b086ef
filiperak <filip.erak@googlemail.com>
2024-05-30 19:49:59 +0200

    slug/routes

--- server.js
+++ server.js
@@ -2,32 +2,18 @@ const express = require("express");
 const app = express();
 const articleRouter = require("./routes/article");
 const mongoose = require('mongoose');
+const Article = require('./models/article')
 
 mongoose.connect('mongodb://localhost:27017/blog').then(console.log('connected to db3'))
 
 app.set("view engine", "ejs");
-app.use("/articles", articleRouter);
 app.use(express.urlencoded({extended : false}))
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "test article",
-      createdAt:new Date(),
-      description: "Test description",
-    },
-    {
-      title: "test article2",
-      createdAt:new Date(),
-      description: "Test description2",
-    },
-    {
-      title: "test article3",
-      createdAt:new Date(),
-      description: "Test description3",
-    },
-  ];
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({
+    createdAt:'desc'
+  })
   res.render("articles/index", { articles: articles });
 });
-
+app.use("/articles", articleRouter);
 app.listen(5000);
