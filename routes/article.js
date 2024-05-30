58b8a79a0dbb5100eebcd2e048266a50354c69b5
filiperak <filip.erak@googlemail.com>
2024-05-30 20:13:44 +0200

    routes

--- routes/article.js
+++ routes/article.js
@@ -5,6 +5,10 @@ const Article = require("./../models/article");
 router.get("/new", (req, res) => {
   res.render("articles/new", { article: new Article() });
 });
+router.get("/edit/:id", async (req, res) => {
+    const article = await Article.findById(req.params.id)
+  res.render("articles/new", { article:article });
+});
 
 router.get("/:slug", async (req, res) => {
   const article = await Article.findOne({
@@ -30,4 +34,10 @@ router.post("/", async (req, res) => {
     res.render("articles/new", { article: article });
   }
 });
+
+
+router.delete('/:id', async (req,res) => {
+    await Article.findByIdAndDelete(req.params.id)
+    res.redirect('/')
+})
 module.exports = router;
--- server.js
+++ server.js
@@ -3,11 +3,13 @@ const app = express();
 const articleRouter = require("./routes/article");
 const mongoose = require('mongoose');
 const Article = require('./models/article')
+const methodOverride = require('method-override')
 
 mongoose.connect('mongodb://localhost:27017/blog').then(console.log('connected to db3'))
 
 app.set("view engine", "ejs");
 app.use(express.urlencoded({extended : false}))
+app.use(methodOverride('_method'))
 
 app.get("/", async (req, res) => {
   const articles = await Article.find().sort({
