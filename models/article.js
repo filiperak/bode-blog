e5a7b8bf662c6f9dbdaeed2136a12de529b5bf6c
filiperak <filip.erak@googlemail.com>
2024-05-31 13:12:50 +0200

    test

--- models/article.js
+++ models/article.js
@@ -1,46 +1,39 @@
-const mongoose = require('mongoose')
-const marked = require('marked')
-const slugify = require('slugify')
-const createDomPuryfy = require('dompurify')
-const {JSDOM} =  require('jsdom')
-const dompurify = createDomPuryfy(new JSDOM().window)
+const mongoose = require('mongoose');
+const marked = require('marked');
+const slugify = require('slugify');
+const createDomPurify = require('dompurify');
+const { JSDOM } = require('jsdom');
+const dompurify = createDomPurify(new JSDOM().window);
 
 const articleSchema = new mongoose.Schema({
-    title:{
-        type:String,
-        required:true
+    title: {
+        type: String,
+        required: true
     },
-    description:{
-        type:String,
+    description: {
+        type: String,
     },
-    markdown:{
-        type:String,
-        required:false
+    markdown: {
+        type: String,
+        required: false
     },
-    createdAt:{
-        type:Date,
+    createdAt: {
+        type: Date,
         default: Date.now
     },
-    slug:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    sanitizedhtml:{
-        type:String,
-        required:true
+    slug: {
+        type: String,
+        required: true,
+        unique: true
     }
+});
 
-})
-
-articleSchema.pre('validate', function(next){
-    if(this.title) {
-        this.slug = slugify(this.title,{lower:true,strict:true})
+articleSchema.pre('validate', function(next) {
+    if (this.title) {
+        this.slug = slugify(this.title, { lower: true, strict: true });
     }
-    if(this.markdown){
-        this.sansanitizedhtmlitizedhttml = dompurify.sanitize(marked(this.markdown))
-    }
-    next()
-})
+    
+    next();
+});
 
-module.exports = mongoose.model('Article',articleSchema)
+module.exports = mongoose.model('Article', articleSchema);
